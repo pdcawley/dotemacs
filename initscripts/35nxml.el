a5b0382bc33b0350b0d86adb4e1e0ab1572f8a11
Piers Cawley <pdcawley@bofh.org.uk>
2008-12-07 06:25:10 +0000

    Tweaks to nxhtml settings

--- initscripts/35nxml.el
+++ initscripts/35nxml.el
@@ -1,4 +1,9 @@
 ;; 35nxml.el -- configuration for nxml-mode
 (load "nxhtml/autostart")
 
+(setq mumamo-chunk-coloring 'submode-colored
+      nxhtml-skip-welcome t
+      indent-region-mode t
+      rng-nxml-auto-validate-flag nil)
+
 ;; 35nxml.el ends
