d834d9643e640f4d96c3806cd481e3516dc64ae2
Piers Cawley <pdcawley@bofh.org.uk>
2011-06-04 07:36:24 +0100

    Added clojure-mode to the automode alist

--- init.el
+++ init.el
@@ -48,3 +48,14 @@ If no argument and at end of line, the previous two chars are exchanged."
 
 (set-face-attribute 'default nil
                     :height 160)
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
---
+++ initscripts/44clojure.el
@@ -0,0 +1,2 @@
+(require 'clojure-mode)
+(add-to-list 'auto-mode-alist '("\\.clj\$" . clojure-mode)
