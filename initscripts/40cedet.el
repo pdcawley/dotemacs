7c17b4380ffde85eb1a7931c5a3116cc45107447
Piers Cawley <pdcawley@bofh.org.uk>
2013-02-18 16:17:16 +0000

    Arduino support

--- initscripts/40cedet.el
+++ initscripts/40cedet.el
@@ -6,7 +6,19 @@
 ;(require 'semantic)
 ;(require 'semantic-wisent)
 ;(require 'semanticdb)
-;(semantic-load-enable-code-helpers)
+                                        ;(semantic-load-enable-code-helpers)
+;(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
+;(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
+;(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+
+;(semantic-mode 1)
+;(global-ede-mode nil)
+;(setq ede-arduino-appdir "/Applications/Arduino.app/Contents/Resources/Java")
+
+(add-to-list 'load-path (concat externals-dir "arduino-mode/"))
+(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+(autoload 'arduino-mode "arduino-mode" "Arduino editing mode" t)
+
 
 ;; ECB
 ;(require 'ecb)
