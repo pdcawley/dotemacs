5e7b734b13e8dbad5ca0f8aac69f5ebf05ed70ed
Piers Cawley <pdcawley@bofh.org.uk>
2013-02-18 15:54:39 +0000

    Updates for multicursor changes

--- initscripts/11multicursors.el
+++ initscripts/11multicursors.el
@@ -2,24 +2,22 @@
 (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
 (require 'mark-more-like-this)
-(global-set-key (kbd "C-<") 'mark-previous-like-this)
-(global-set-key (kbd "C->") 'mark-next-like-this)
-(global-set-key (kbd "C-M-m") 'mark-more-like-this)
-(global-set-key (kbd "C-*") 'mark-all-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-M-m") 'mc/mark-more-like-this)
+(global-set-key (kbd "C-*") 'mc/mark-all-like-this)
 
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 
-(require 'mc-mark-multiple-integration)
+;; (defun pdc/edit-marks-or-lines ()
+;;   (interactive)
+;;   (if mm/master
+;;       (mc/switch-from-mark-multiple-to-cursors)
+;;     (mc/edit-lines)))
 
-(defun pdc/edit-marks-or-lines ()
-  (interactive)
-  (if mm/master
-      (mc/switch-from-mark-multiple-to-cursors)
-    (mc/edit-lines)))
-
-(global-set-key (kbd "C-S-c C-S-c") 'pdc/edit-marks-or-lines)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 
 ;;  (defun mc/enable-function (func)
 ;;   "Marks a function as enabled during multi cursor operations"
