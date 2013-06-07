bd349d77aa6f9782520155724924b1ef38ee5a79
Piers Cawley <pdcawley@bofh.org.uk>
2013-06-07 11:27:14 +0100

    Added code to load required packages

---
+++ initscripts/01packages.el
@@ -0,0 +1,35 @@
+(defvar prelude-packages
+  '(ack-and-a-half align-cljlet anything apache-mode
+    asciidoc browse-url-dwim buffer-move clojure-mode coffee-mode
+    dropbox el-autoyas erefactor fastnav flymake flymake-easy flymake-haml
+    flymake-haskell-multi flymake-hlint flymake-perlcritic flymake-sass
+    flymake-sass flymake-shell fringe-helper gist git-commit-mode git-gutter
+    git-gutter-fringe gitignore-mode gitty go-mode goto-last-change haml-mode
+    haskell-mode
+    js2-mode json json-mode
+    keyfreq less-css-mode lexbind-mode list-utils load-dir
+    log4j-mode logito
+    magit magit-gh-pulls magithub markdown-mode mediawiki mo-git-blame
+    multi-term nav nterm nzenburn-theme org org-journal org-magit 
+    paredit pastebin pcache pcmpl-args perlbrew persistent-soft
+    project projectile scss-mode string-utils tt-mode typing ucs-utils
+    undo-tree unicode-enbox wgrep wgrep-ack winpoint yagist yaml-mode
+    zencoding-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'prelude-packages)
