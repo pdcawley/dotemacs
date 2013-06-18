550d9418423d9f8352eb2ee0bf571ae577eb62d2
Piers Cawley <pdcawley@bofh.org.uk>
2013-06-18 12:15:08 +0100

    Made some cperl-mode changes

--- initscripts/50perl.el
+++ initscripts/50perl.el
@@ -1,6 +1,7 @@
 ;; Use cperl mode instead of the default perl mode
 
 (add-to-list 'load-path (concat dotfiles-dir "/cperl-mode"))
+(require 'cperl-mode)
 
 (defalias 'perl-mode 'cperl-mode)
 (add-to-list 'auto-mode-alist '("\\.t\\'" . perl-mode))
@@ -40,3 +41,39 @@
     (skip-chars-forward " \t\n")
     (if (memq sexp-start (append "([" nil))
         (backward-char cperl-continued-statement-offset))))
+
+(defun pdc/indent-cperl-indentable (i parse-data)
+  (cond            ;;; [indentable terminator start-pos is-block]
+   ((eq 'terminator (elt i 1)) ; Lone terminator of "indentable string"
+    (goto-char (elt i 3))   ; prev line
+    (current-indentation))
+   ((eq 'first-line (elt i 1)); [indentable first-line start-pos]
+    (goto-char (elt i 2))
+    (+ cperl-continued-statement-offset
+       (current-indentation)))
+   ((eq 'cont-line (elt i 1)); [indentable cont-line pos prev-pos first-char start-pos]
+    ;; Indent as the level after closing parens
+    (goto-char (elt i 2))   ; indent line
+    (skip-chars-forward " \t)") ; Skip closing parens
+    (setq p (point))
+    (goto-char (elt i 3))   ; previous line
+    (skip-chars-forward " \t)") ; Skip closing parens
+    ;; Number of parens in between:
+    (setq p (nth 0 (parse-partial-sexp (point) p))
+          what (elt i 4))   ; First char on current line
+    (goto-char (elt i 3))   ; previous line
+    (+ (* p (or cperl-regexp-indent-step cperl-indent-level))
+       (cond ((eq what ?\) )
+              (- cperl-close-paren-offset)) ; compensate
+             ((eq what ?\| )
+              (- (or cperl-regexp-indent-step cperl-indent-level)))
+             (t 0))
+       (if (eq (following-char) ?\| )
+           (or cperl-regexp-indent-step cperl-indent-level)
+         0)
+       (current-column)))
+   (t
+    (error "Unrecognized value of indent: %s" i))))
+
+(push '(indentable pdc/indent-cperl-indentable)
+      cperl-indent-rules-alist)
