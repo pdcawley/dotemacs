da6b8631c59184a59a87c89ab8da85f3ad792528
Piers Cawley <pdcawley@bofh.org.uk>
2014-03-18 10:28:03 +0000

    Some slight reorganisation and some new submodules.

--- initscripts/ibuffer-setup.el
+++
@@ -1,7 +0,0 @@
-(use-package ibuffer
-  :defer t
-  :bind ("C-x C-b" . ibuffer)
-  :init
-  (add-hook 'ibuffer-mode-hook
-            #'(lambda ()
-                (ibuffer-switch-to-saved-filter-groups "default"))))
