d2381cfeb5ab7c434f1d639e9c9ec6d3a3107cc6
Piers Cawley <pdcawley@bofh.org.uk>
2015-07-31 02:19:49 +0100

    Move el-get-initialization to a 'require'd file
    Steps towards making the initial part of our initialization (package
    loading, path setting etc, the initscripts/00.* stuff into a slightly
    less magical load sequence. initscripts should really be about
    configuring optional packages that have been installed, not about
    installing said packages.
    
    Also, moving towards using el-get as the sole mechanism for managing
    packages.

---
+++ elisp/pdc-packages-and-paths.el
@@ -0,0 +1,289 @@
+;;; bootstrap
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil t)
+  (with-current-buffer (url-retrieve-synchronously
+                        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)
+    (load-library "el-get")))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes/")
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes/")
+
+;;; see https://github.com/purcell/emacs.d for more ideas
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (el-get-elpa-build-local-recipes)
+;;(require 'dash)
+;;; load packages
+(setq el-get-byte-compile t
+      el-get-use-autoloads t
+      el-get-sources
+      '((:name desktop-recover :type git :url "https://github.com/doomvox/desktop-recover.git")
+        (:name lineker :type http
+               :url "http://www.helsinki.fi/~sjpaavol/programs/lineker.el")
+        (:name etags-select :type emacswiki)
+
+        )
+      old-el-get-sources '(
+                       (:name gist :type git :url "https://github.com/tels7ar/gist.el")
+                       (:name auto-complete
+                              :website "http://cx4a.org/software/auto-complete/"
+                              :description "The most intelligent auto-completion extension."
+                              :type git
+                              :url "http://github.com/auto-complete/auto-complete.git"
+                              :load-path "."
+                              :post-init (progn
+                                           (require 'auto-complete)
+                                           (add-to-list 'ac-dictionary-directories (expand-file-name "dict" pdir))
+                                           ;; the elc is buggy for some reason
+                                           (let ((f "~/.emacs.d/el-get/auto-complete/auto-complete-config.elc"))
+                                             (if (file-exists-p f)
+                                                 (delete-file f)))
+                                           (require 'auto-complete-config)
+                                           (ac-config-default)
+                                           ))
+
+                       (:name workgroups :type git :url "https://github.com/tlh/workgroups.el.git")
+                       (:name monky :type git
+                              :url "https://github.com/ananthakumaran/monky.git"
+                              :build ("make all"))
+                       (:name isearch+ :type emacswiki)
+                       (:name linkd :type emacswiki)
+                       (:name grep-a-lot :type git :url
+                              "https://github.com/ZungBang/emacs-grep-a-lot")
+                       (:name idle-highlight-mode :type git :url
+                              "https://github.com/nonsequitur/idle-highlight-mode.git")
+                       (:name list-register :type http :url "http://www.bookshelf.jp/elc/list-register.el")
+                       (:name bm :type http :url "http://cvs.savannah.gnu.org/viewvc/*checkout*/bm/bm/bm.el")
+                       (:name window-numbering :type git :url "https://github.com/nschum/window-numbering.el.git")
+                       (:name column-marker :type http
+                              :url "http://www.emacswiki.org/emacs/download/column-marker.el")
+                       (:name col-highlight :type emacswiki)
+                       (:name eredis :type http :url "http://eredis.googlecode.com/svn/trunk/eredis.el")
+                       (:name ac-dabbrev :type emacswiki)
+                       (:name cobol-mode :type emacswiki)
+                       ;;
+                       (:name show-wspace :type emacswiki)
+                       (:name visible-mark :type emacswiki)
+                       (:name transpose-frame :type emacswiki)
+                       (:name relax :type git :url "https://github.com/technomancy/relax.el")
+                       (:name moz :type git :url "http://github.com/bard/mozrepl.git"
+                              :load "chrome/content/moz.el")
+                       (:name rainbow-delimiters :type git :url "https://github.com/jlr/rainbow-delimiters.git")
+                       (:name rainbow-mode
+                              :type git
+                              :url "https://github.com/emacsmirror/rainbow-mode.git")
+                       (:name js2-mode :type git :url "https://github.com/mooz/js2-mode")
+                       (:name jquery-doc :type git :url "git://github.com/ananthakumaran/jquery-doc.el.git")
+
+                       (:name flymake-coffee :type git :url "git://github.com/purcell/flymake-coffee.git")
+                       (:name flymake-node-jshint :type git :url "git://github.com/jegbjerg/flymake-node-jshint.git")
+                       (:name elein :type git :url "https://github.com/remvee/elein.git")
+                       (:name pg :type http :url "http://www.online-marketwatch.com/pgel/pg.el")
+                       (:name stompem :type git :url "https://github.com/jwhitlark/Stompem.git")
+
+
+                       (:name elnode :type git :url "https://github.com/tavisrudd/elnode.git")
+                       (:name gh :type git :url "https://github.com/sigma/gh.el.git")
+                       (:name pcache :type git :url "https://github.com/sigma/pcache.git")
+                       (:name auto-complete-etags :type git :url "https://github.com/whitypig/auto-complete-etags.git")
+
+                       (:name restclient :type git :url "https://github.com/pashky/restclient.el.git")
+                       (:name buster-mode :type git :url "git://gitorious.org/buster/buster-mode.git")
+                       (:name mark-multiple :type git :url "git://github.com/magnars/mark-multiple.el.git")
+                       (:name aws-el :type git :url "https://github.com/ieure/aws-el.git")
+
+                       (:name helm :type git :url "https://github.com/emacs-helm/helm.git")
+
+                       (:name evil-surround
+                              :type git
+                              :url "https://github.com/timcharper/evil-surround.git")
+                       (:name clojure-mode :type git :url "https://github.com/tavisrudd/clojure-mode.git")
+      ))
+
+
+
+(setq dss-el-get-packages
+      '(
+        ;; ac-dabbrev
+        ;; ac-slime
+
+        ido-vertical-mode
+        ido-ubiquitous
+        ido-hacks
+        projectile
+        geiser
+        web-mode
+        elisp-slime-nav
+        redshank
+        yasnippet
+        yasnippets
+        yasnippet-snippets
+        ace-jump-mode
+        ack-and-a-half
+        align-cljlet
+        anything
+        apache-mode
+        asciidoc
+        auto-complete
+        auto-complete-etags
+        auto-complete-extension
+        autopair
+        bm
+        breadcrumb
+        browse-kill-ring
+        buffer-move
+        clojure-mode
+        coffee-mode
+        col-highlight
+        color-theme
+        column-marker
+        dash
+        desktop-recover
+        diminish
+        el-autoyas
+        elein
+        emacs-jabber
+       ;; ess
+       ;; etags-select
+        evil
+        evil-surround
+        exec-path-from-shell
+        f
+        fastnav
+        filladapt
+        flymake
+        flymake-coffee
+        flymake-easy
+        flymake-haml
+        flymake-sass
+        flymake-shell
+        ;; fm
+        fringe-helper
+        gist
+        git-gutter
+        git-gutter-fringe
+        ;; go-mode
+        goto-last-change
+        haml-mode
+        haskell-mode
+        helm
+        hide-comnt
+        highlight-symbol
+        highlight-cl
+        hlinum
+        ibuffer-vc
+        idle-highlight-mode
+        iedit
+        isearch+
+        jquery-doc
+        js2-mode
+        json
+        json-mode
+        keyfreq
+        less-css-mode
+        ;; lexbind-mode
+        lineker
+        list-register
+        list-utils
+        logito
+        magit
+        magit-gitflow
+        monky
+        mark-multiple
+        markdown-mode
+        mediawiki
+        mo-git-blame
+        monky
+        ;; moz
+        multi-term
+        nav
+        nterm
+        ;; nzenburn-theme
+        ;; org-journal
+        org-magit
+        org-mode
+        paredit
+        ;; pastebin
+        pbcopy
+        pcache
+        ;; pcmpl-args
+        pcre2el
+        ;; perlbrew
+        persistent-soft
+        per-window-point
+        pg
+        popup
+        popup-kill-ring
+        pos-tip
+        pos-tip ;; required by popup-kill-ring
+        powerline
+        pretty-mode
+        ;; project
+        protbuf
+        rainbow-delimiters
+        rainbow-mode
+        ;; rect+
+        restclient
+        s
+        ;; s-buffer
+        ;; sackspace
+        scss-mode
+        session
+        highlight-chars
+        ;; slime
+        ;; slime-js
+        smex
+        ;; ssh-config-mode
+        ;; string-inflection
+        string-utils
+        swift-mode
+        tabulated-list
+        tagedit
+        thing-cmds
+        thingatpt+
+        transpose-frame
+        ;; trie
+        ;; tt-mode
+        ucs-utils
+        unbound
+        undo-tree
+        ;; unfill
+        unicode-enbox
+        use-package
+        ;; unipoint
+        ;; virtualenv
+        ;; virtualenv ;;pylookup
+        visible-mark
+        visual-regexp
+        vline
+        ;; whitespace-cleanup-mode
+        whole-line-or-region
+        window-numbering
+        winpoint
+        workgroups
+        yagist
+        yaml-mode
+        zencoding-mode))
+
+(unless (require 'filladapt nil t)
+  (add-to-list 'dss-el-get-packages 'filladapt))
+
+(defun el-get-update-all ()
+  "Update all el-get packages
+  This was copied from https://github.com/purcell/emacs.d/blob/master/init-el-get.el"
+  (interactive)
+  (dolist (package dss-el-get-packages)
+    (unless (memq (plist-get (el-get-package-def package) :type) '(http-tar elpa))
+      (el-get-update package))))
+
+(let ((el-get-verbose t))
+  (el-get 'sync dss-el-get-packages))
+
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+(provide 'pdc-packages-and-paths)
+;;;
--- init.el
+++ init.el
@@ -1,12 +1,10 @@
-(setq package-archives
-      '(("gnu"       . "http://elpa.gnu.org/packages/")
-        ("original"  . "http://tromey.com/elpa")
-        ("org"       . "http://orgmode.org/elpa/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")
-        ("melpa"     . "http://melpa.milkbox.net/packages/")))
 
-(setq package-enable-at-start nil)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
+
 (setq message-log-max 16384)
 (defconst emacs-start-time (current-time))
 ;; encoding
@@ -16,20 +14,23 @@
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+(add-to-list 'load-path (expand-file-name "elisp/" user-emacs-directory))
+(require 'pdc-packages-and-paths)
+
 (defvar lisp-modes '(emacs-lisp-mode
-                     inferior-emacs-lisp-mode
-                     ielm-mode
-                     lisp-mode
-                     inferior-lisp-mode
-                     lisp-interaction-mode
-                     slime-repl-mode))
+             inferior-emacs-lisp-mode
+             ielm-mode
+             lisp-mode
+             inferior-lisp-mode
+             lisp-interaction-mode
+             slime-repl-mode))
 
 (defvar lisp-mode-hooks
-  (mapcar (function
-           (lambda (mode)
-             (intern
-              (concat (symbol-name mode) "-hook"))))
-          lisp-modes))
+(mapcar (function
+   (lambda (mode)
+     (intern
+      (concat (symbol-name mode) "-hook"))))
+  lisp-modes))
 
 
 (require 'autoinsert)
@@ -37,27 +38,27 @@
 (require 'saveplace)
 (require 'ffap)
 
-(defvar pdc/required-packages
-  (list 'yasnippet 'use-package))
+;; (defvar pdc/required-packages
+;;   (list 'yasnippet 'use-package))
 
-(let ((yas-bundle-desc (assq 'yasnippet-bundle package-alist)))
-  (when yas-bundle-desc
-    (package-delete "yasnippet-bundle"
-                    (package-version-join
-                     (package-desc-vers (cdr yas-bundle-desc))))))
+;; (let ((yas-bundle-desc (assq 'yasnippet-bundle package-alist)))
+;;   (when yas-bundle-desc
+;;     (package-delete "yasnippet-bundle"
+;;                     (package-version-join
+;;                      (package-desc-vers (cdr yas-bundle-desc))))))
 
-(unless package-archive-contents
-  (package-refresh-contents))
-(dolist (package pdc/required-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+;; (dolist (package pdc/required-packages)
+;;   (unless (package-installed-p package)
+;;     (package-install package)))
 
 (load (expand-file-name "load-path" (file-name-directory load-file-name)))
 
 ;;; Work around a bug on OS X where system-name is FQDN.
 (if (or (eq system-type 'darwin)
-        (eq system-type 'berkeley-unix))
-    (setq system-name (car (split-string system-name "\\."))))
+(eq system-type 'berkeley-unix))
+(setq system-name (car (split-string system-name "\\."))))
 
 
 
@@ -73,61 +74,61 @@
 ;;(load custom-file)
 
 (defun remove-extension (name)
-             (string-match "\\(.*?\\)\.\\(org\\(\\.el\\)?\\|el\\)\\(\\.gpg\\)?$" name)
-             (match-string 1 name))
+     (string-match "\\(.*?\\)\.\\(org\\(\\.el\\)?\\|el\\)\\(\\.gpg\\)?$" name)
+     (match-string 1 name))
 
 ;;; * Load User/System Specific Files
 (cl-flet ((sk-load (base)
-         (let* ((path          (expand-file-name base user-emacs-directory))
-                (literate      (concat path ".org"))
-                (encrypted-org (concat path ".org.gpg"))
-                (plain         (concat path ".el"))
-                (encrypted-el  (concat path ".el.gpg")))
-           (cond
-            ((file-exists-p encrypted-org) (org-babel-load-file encrypted-org))
-            ((file-exists-p encrypted-el)  (load encrypted-el))
-            ((file-exists-p literate)      (org-babel-load-file literate))
-            ((file-exists-p plain)         (load plain)))))
-       (remove-extension (name)
-         (string-match "\\(.*?\\)\.\\(org\\(\\.el\\)?\\|el\\)\\(\\.gpg\\)?$" name)
-         (match-string 1 name)))
-      (let ((user-dir (expand-file-name user-login-name user-emacs-directory)))
-        (dolist (default-directory (nreverse
-                                    (list user-override-directory
-                                          user-lisp-directory
-                                          user-lib-directory
-                                          user-site-lisp-directory
-                                          user-external-lisp-directory)))
-          (when (file-exists-p default-directory)
-            (normal-top-level-add-subdirs-to-load-path)))
-        (sk-load system-name)
-        (sk-load user-login-name)
-        (dolist (dir (list user-dir user-initscripts-directory))
-          (when (and (file-exists-p dir)
-                     (not (equal (file-name-as-directory
-                                  (expand-file-name user-emacs-directory))
-                                 dir)))
-            (add-to-list 'load-path dir)
-            (mapc (lambda (x) (sk-load x))
-                  (remove-duplicates
-                   (mapcar #'remove-extension
-                           (directory-files dir t ".*\.\\(org\\|el\\)\\(\\.gpg\\)?$"))
-                   :test #'string=))))))
+ (let* ((path          (expand-file-name base user-emacs-directory))
+        (literate      (concat path ".org"))
+        (encrypted-org (concat path ".org.gpg"))
+        (plain         (concat path ".el"))
+        (encrypted-el  (concat path ".el.gpg")))
+   (cond
+    ((file-exists-p encrypted-org) (org-babel-load-file encrypted-org))
+    ((file-exists-p encrypted-el)  (load encrypted-el))
+    ((file-exists-p literate)      (org-babel-load-file literate))
+    ((file-exists-p plain)         (load plain)))))
+(remove-extension (name)
+ (string-match "\\(.*?\\)\.\\(org\\(\\.el\\)?\\|el\\)\\(\\.gpg\\)?$" name)
+ (match-string 1 name)))
+(let ((user-dir (expand-file-name user-login-name user-emacs-directory)))
+(dolist (default-directory (nreverse
+                            (list user-override-directory
+                                  user-lisp-directory
+                                  user-lib-directory
+                                  user-site-lisp-directory
+                                  user-external-lisp-directory)))
+  (when (file-exists-p default-directory)
+    (normal-top-level-add-subdirs-to-load-path)))
+(sk-load system-name)
+(sk-load user-login-name)
+(dolist (dir (list user-dir user-initscripts-directory))
+  (when (and (file-exists-p dir)
+             (not (equal (file-name-as-directory
+                          (expand-file-name user-emacs-directory))
+                         dir)))
+    (add-to-list 'load-path dir)
+    (mapc (lambda (x) (sk-load x))
+          (remove-duplicates
+           (mapcar #'remove-extension
+                   (directory-files dir t ".*\.\\(org\\|el\\)\\(\\.gpg\\)?$"))
+           :test #'string=))))))
 
 
 (setq custom-file (expand-file-name "preferences.el" user-emacs-directory))
 (load-file custom-file)
 
 (when window-system
-  (let ((elapsed (float-time (time-subtract (current-time)
-                                            emacs-start-time))))
-    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
+(let ((elapsed (float-time (time-subtract (current-time)
+                                    emacs-start-time))))
+(message "Loading %s...done (%.3fs)" load-file-name elapsed))
 
-  (add-hook 'after-init-hook
-            `(lambda ()
-               (let ((elapsed (float-time (time-subtract (current-time)
-                                                         emacs-start-time))))
-                 (message "Loading %s...done (%.3fs) [after-init]"
-                          ,load-file-name elapsed)))
-            t))
+(add-hook 'after-init-hook
+    `(lambda ()
+       (let ((elapsed (float-time (time-subtract (current-time)
+                                                 emacs-start-time))))
+         (message "Loading %s...done (%.3fs) [after-init]"
+                  ,load-file-name elapsed)))
+    t))
 (put 'dired-find-alternate-file 'disabled nil)
