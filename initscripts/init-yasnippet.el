2f605f7286dd5103500e58a23309800298c3041f
Piers Cawley <pdcawley@bofh.org.uk>
2017-07-31 17:29:15 +0100

    Fiddle with the binding declarations.

--- initscripts/init-yasnippet.el
+++ initscripts/init-yasnippet.el
@@ -1,16 +1,19 @@
+;;; -*- lexical-binding: t; -*-
+
 (eval-when-compile (require 'pdc-utils))
 
 (req-package yasnippet
-  :ensure t
   :if (not noninteractive)              ; no point in batch mode
-  :bind
-  (("C-c y TAB" . yas-expand)
-   ("C-c y C-TAB" . yas-next-field)
-   ("C-c y <C-tab>" . yas-next-field)
-   ("C-c y n" . yas-new-snippet)
-   ("C-c y f" . yas-find-snippets)
-   ("C-c y r" . yas-reload-all)
-   ("C-c y v" . yas-visit-snippet-file))
+  :general
+  (pdc|with-leader
+   "y" '(nil "snippet")
+   "y TAB" 'yas-expand
+   "y C-TAB" 'yas-next-field
+   "y <C-tab>" 'yas-next-field
+   "y n" 'yas-new-snippet
+   "y f" 'yas-find-snippets
+   "y r" 'yas-reload-all
+   "y v" 'yas-visit-snippet-file)
   :diminish (yas-minor-mode " Ⓨ")
   :commands
   (yas-minor-mode yas-expand)
@@ -18,9 +21,23 @@
   ("/\\.emacs\\.d/snippets/" . snippet-mode)
   :config
   (progn
-                                        ; Load local snippets
+    ; Load local snippets
     (yas-load-directory
      (expand-file-name "snippets/" user-emacs-directory))
-                                        ; ... and use them everywhere
+    ; ... and use them everywhere
     (yas-global-mode t)))
+
+(req-package autoinsert
+  :requires yasnippet
+  :init
+  (setq auto-insert-query nil)
+  (setq auto-insert-directory (locate-user-emacs-file "templates"))
+  (auto-insert-mode 1)
+  (defun pdc/autoinsert-yas-expand ()
+    "Replace text in yasnippet template"
+    (yas-expand-snippet (buffer-string) (point-min) (point-max)))
+
+  :config
+  (define-auto-insert "\\.html?$" ["default-html.html" pdc/autoinsert-yas-expand])
+  (define-auto-insert "\\.el$" [ "default-elisp.el" pdc/autoinsert-yas-expand ]))
 
