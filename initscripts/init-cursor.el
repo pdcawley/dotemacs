36f90ca6ddd247c52bf711bd873deea5e6d9fe26
Piers Cawley <pdcawley@bofh.org.uk>
2017-07-31 16:26:01 +0100

    Add s-<mouse-1> binding for adding a multi cursor

--- initscripts/init-cursor.el
+++ initscripts/init-cursor.el
@@ -1,14 +1,33 @@
 (eval-when-compile (require 'req-package))
 
 (req-package multiple-cursors
-  :bind
-  (("C-S-c" . nil)
-   ("C-S-c C-S-c" . mc/edit-lines)
-   ("C-S-c C-e" . mc/edit-ends-of-lines)
-   ("C-S-c C-a" . mc/edit-beginnings-of-lines)
-   ("C-. m e" . mc/edit-lines)
-   ("C-. m >" . mc/mark-next-like-this)
-   ("C-. m <" . mc/mark-previous-line-like-this)
-   ("C-. m a" . mc/mark-all-like-this)))
+  :general
+  ("s-<mouse-1>" 'mc/add-cursor-on-click)
+  (:keymaps
+   ctl-x-map
+   "C-m" 'mc/mark-all-dwim
+   "<return>" 'pdc/execute-extended-command-short)
+  :config
+  (with-eval-after-load 'init-leaders
+    (pdc|general-bind-hydra mc "m"
+      ("<" mc/mark-previous-like-this)
+      (">" mc/mark-next-like-this )
+      ("M-<" mc/unmark-previous-like-this)
+      ("M->" mc/unmark-next-like-this)
+      ("i" mc/insert-numbers)
+      ;; ("h" mc/hide-unmatched-lines-mode)
+      ("d" mc/mark-all-symbols-like-this-in-defun :color blue)
+      ("r" mc/reverse-regions)
+      ("s" mc/sort-regions))
+    (pdc|with-leader
+     "m C-a" 'mc/edit-beginnings-of-lines
+     "m A" 'mc/mark-all-like-this
+     "m a" 'mc/mark-all-dwim
+     "m C-e" 'mc/edit-ends-of-lines
+     "m e" 'mc/edit-lines)
+    )
+  )
+"
 
-(provide 'init-cursor)
+
+(provide 'init-cursor)"
