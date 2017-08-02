c0f250341c4838f14e275461e4547a7897e04133
Piers Cawley <pdcawley@bofh.org.uk>
2017-08-02 22:56:23 +0100

    Not quite sure what's happening to init.el
    Seems to be being changed by custom or something

--- init.el
+++ init.el
@@ -87,7 +87,6 @@ Note the weekly scope of the command's precision")
     (my-tangle-config-org))
   (load-file elfile))
 
-
 ;; when config.org is saved, re-generate config.el
 (defun my-tangle-config-org-hook-func ()
   (when (string= "config.org" (buffer-name))
@@ -106,7 +105,7 @@ Note the weekly scope of the command's precision")
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (kv s magit hydra ivy paredit macrostep yankpad yasnippet req-package no-littering general))))
+    (window-numbering ibuffer-vc kv s magit hydra ivy paredit macrostep yankpad yasnippet req-package no-littering general))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
