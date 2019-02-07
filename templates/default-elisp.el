97b20c027a333ff256b545e583ed816a14bcea48
Piers Cawley <pdcawley@bofh.org.uk>
2019-02-07 11:30:53 +0000

    template like updates

---
+++ templates/default-elisp.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t; -*-
+;;; ${1:`(file-name-nondirectory (file-name-sans-extension (buffer-file-name)))`} -- ${2:description}
+;;
+;; Author: `user-full-name` <`user-mail-address`>
+${3:;;
+;;; Commentary;
+;;
+;;  $4
+};;
+;;; Code:
+
+$0
+
+(provide '$1)
+
+;;; $1 ends here
