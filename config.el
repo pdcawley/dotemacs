6e5aca01279e0d3febae5df88a1a98a426e0a6f4
Piers Cawley <pdcawley@bofh.org.uk>
2022-05-24 10:15:19 +0100

    properly import the pdcmacs config reboot

---
+++ config.el
@@ -0,0 +1,458 @@
+(eval-when-compile
+  (require 'general)
+  (require 'use-package))
+
+(add-hook 'after-init-hook #'(lambda ()
+                               (interactive)
+                               (require 'server)
+                               (or (server-running-p)
+                                   (server-start))))
+
+;;;
+;;; Performance
+(if (and (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+    (setq comp-deferred-compilation t
+          package-native-compile t)
+  (message "No native compilation available"))
+
+(setq warning-suppress-types '((comp)))
+
+
+
+;;
+;; More or less sensible defaults
+
+;; (global-eldoc-mode 1)
+(electric-pair-mode 1)
+(add-hook 'before-save-hook 'whitespace-cleanup)
+(delete-selection-mode)
+
+(setq-default
+ frame-resize-pixelwise t
+
+ indent-tabs-mode nil
+ tab-width 4
+ fill-column 79
+
+ gnutls-verify-error t
+ gnutls-min-prime-bits 2048
+
+ password-cache-expiry nil
+
+ mouse-yank-at-point t
+ save-interprogram-paste-before-kill t
+ apropos-do-all t
+ require-final-newline t
+ ediff-window-setup-function 'ediff-setup-windows-plain
+
+ tramp-default-method "ssh"
+ tramp-copy-size-limit nil
+ tramp-use-ssh-controlmaster-options nil
+
+ vc-follow-symlinks t
+
+ ring-bell-function 'ignore)
+
+(add-hook 'dired-load-hook (function (lambda () (load "dired-x"))))
+
+
+;; Revert dired and other buffers
+(customize-set-variable 'global-auto-revert-non-file-buffers t)
+
+
+;; Revert buffers when the underlying file changed.
+(global-auto-revert-mode 1)
+
+;; Spaces, not tabs.
+(setq-default indent-tabs-mode nil
+              completions-detailed t
+              read-minibuffer-restore-windows nil
+              mode-line-compact 'long)
+
+
+;; #'yes-or-no-p can die in a fire
+(setq-default use-short-answers t)
+;; Turn on recentf mode
+(add-hook 'after-init-hook #'recentf-mode)
+(setq recentf-save-file (expand-file-name "recentf" pdcmacs-var-directory))
+
+;; savehist mode
+(setq history-length 25)
+(savehist-mode 1)
+
+;; Don't stick duplicates in kill-ring
+(customize-set-variable 'kill-do-not-save-duplicates t)
+
+;; Make scrolling a bit less stuttery
+(setq auto-window-vscroll nil)
+(customize-set-variable 'fast-but-imprecises-scrolling t)
+(customize-set-variable 'scroll-conservatively 101)
+(customize-set-variable 'scroll-margin 0)
+(customize-set-variable 'scroll-preserve-screen-position t)
+
+
+;; Better support for files with long names
+(setq-default bidi-paragraph-direction 'left-to-right)
+(setq-default bidi-inhibit-bpa t)
+(global-so-long-mode 1)
+
+;; Make shebang files executable on save
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+;; Diminish some annoying stuff
+(diminish 'eldoc-mode "")
+
+;; sentences do not end with a double space
+(setq sentence-end-double-space nil)
+
+;; Enable some stuff
+(put 'narrow-to-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
+
+;;
+;; Editing stuff
+
+;; trim excess whitespace
+(use-package ws-butler
+  :diminish ""
+  :hook
+  ((prog-mode text-mode) . ws-butler-mode))
+
+;; parentheses
+(electric-pair-mode 1)
+(show-paren-mode 1)
+
+(use-package paredit
+  :diminish "Ⓟ "
+  :hook
+  ((lisp-mode scheme-mode racket-mode emacs-lisp-mode) . enable-paredit-mode))
+
+(use-package mwim
+  :custom
+  (mwim-position-functions '(mwim-code-beginning
+                             mwim-line-beginning
+                             mwim-comment-beginning
+                             +mwim-current-string-beginning
+                             +mwim-current-string-end
+                             mwim-code-end
+                             mwim-line-end))
+  :general
+  (general-def
+    "C-a" 'mwim-beginning
+    "C-e" '+mwim-next-ending)
+  :config
+  (defun +mwim-next-ending ()
+    "Move point to the the nearest ending place"
+    (interactive "^")
+    (mwim-move-to-next-position mwim-end-position-functions #'<))
+
+  (defun +mwim-current-string-beginning ()
+    "Return position of the beginning of the current string.
+Return nil if not inside a string."
+    (let ((syn (syntax-ppss)))
+      (and (nth 3 syn)
+           (nth 8 syn))))
+
+  (defun +mwim-beginning-of-current-string ()
+    "Move point of the beginning of the current string.
+If we're not in a string, do nothing."
+    (interactive "^")
+    (when-let ((string-beg (+mwim-current-string-beginning)))
+      (goto-char string-beg)))
+
+  (defun +mwim-current-string-end ()
+    "Return position of the end of the current string.
+Return nil if not inside a string."
+    (mwim-point-at (+mwim-end-of-current-string)))
+
+  (defun +mwim-end-of-current-string ()
+    "Move point to the end of the current string.
+Do nothing if we're not in a string."
+    (interactive "^")
+    (when-let ((string-beg (+mwim-current-string-beginning)))
+      (goto-char string-beg)
+      (forward-sexp)))
+
+  (push '+mwim-current-string-beginning mwim-beginning-position-functions)
+
+  (push '+mwim-current-string-end mwim-end-position-functions))
+
+
+;;
+;; Completion frameworks and such.
+
+(use-package vertico
+  :custom
+  (vertico-cycle t)
+  :config
+  (require 'vertico-directory "extensions/vertico-directory.el")  (vertico-mode 1))
+
+(use-package marginalia
+  :config
+  (setq marginalia-annotators '(marginalia-annotators-light nil))
+  (marginalia-mode 1))
+
+(use-package consult
+  :general
+  ("C-s" 'consult-line)
+  (:keymaps 'minibuffer-local-map
+        "C-r" 'consult-history)
+  :config
+  (setq completion-in-region-function #'consult-completion-in-region))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless))
+  (completion-category-overrides '((file (styles . (partial-completion)))))
+  :config
+  (setq completion-category-defaults nil))
+
+(use-package embark
+  :general
+  ("C-." #'embark-act)
+  ([remap describe-bindings] #'embark-bindings)
+  :config
+  (setq prefix-help-command #'embark-prefix-help-command))
+
+(use-package embark-consult)
+(use-package consult-dir)
+(use-package ace-window)
+(use-package 0x0)
+
+;;; Appearance
+
+(use-package all-the-icons)
+(use-package doom-modeline
+  :custom
+  (doom-modeline-height 15)
+  (doom-modeline-bar-width 6)
+  (doom-modeline-minor-modes t)
+  (doom-modeline-buffer-file-name-style 'truncate-except-project)
+  :init
+  (add-hook 'after-init-hook 'doom-modeline-init))
+
+;; utility libraries
+(use-package dash)
+(use-package s)
+(use-package f)
+(use-package kv)
+(use-package ht)
+
+(eval-when-compile
+  (require 'dash)
+  (require 's)
+  (require 'f)
+  (require 'kv)
+  (require 'ht))
+
+;; Speedup with auto-compile
+;; (use-package auto-compile
+;;   :config
+;;   (auto-compile-on-load-mode)
+;;   (auto-compile-on-save-mode)
+;;   (setq auto-compile-display-buffer nil
+;;         auto-compile-mode-line-counter t))
+
+;; Make `describe-*' screens more helpful
+
+;; (use-package helpful
+;;   :general
+;;   (:keymaps 'helpful-mode-map
+;;             [remap revert-buffer] #'helpful-update)
+;;   ([remap describe-command] #'helpful-command
+;;    [remap describe-function] #'helpful-callable
+;;    [remap describe-key] #'helpful-key
+;;    [remap describe-symbol] #'helpful-symbol
+;;    [remap describe-variable] #'helpful-variable
+;;    "C-h C" #'helpful-command
+;;    "C-h F" #'helpful-function
+;;    "C-h K" #'describe-keymap)
+;;   :config
+
+;;   ;; Temporary fix until this all works for Emacs 29 again
+;;   (defvar read-symbol-positions-list nil)
+;;   (defun helpful--autoloaded-p (sym buf)
+;;     (-when-let (file-name (buffer-file-name buf))
+;;       (setq file-name (s-chop-suffix "*.gz" file-name))
+;;       (help-fns--autoloaded-p sym)))
+
+
+;;   (defun helpful--skip-advice (docstring)
+;;     "Remove mentions of advice from DOCSTRING."
+;;     (let* ((lines (s-lines docstring))
+;;            (relevant-lines
+;;             (--take-while
+;;              (not (or (s-starts-with-p ":around advice:" it)
+;;                       (s-starts-with-p "This function has :around advice:" it)))
+;;              lines)))
+;;       (s-trim (s-join "\n" relevant-lines)))))
+
+;; ;; add visual pulse when changing focus, like beacon but built-in
+;; (defun pulse-line (&rest _)
+;;   "Pulse the current line"
+;;   (pulse-momentary-highlight-one-line (point)))
+
+;; (dolist (command '(scroll-up-command scroll-down-command
+;;                                      recenter-top-bottom other-window))
+;;   (advice-add command :after #'pulse-line))
+
+;;;
+;;; Windows stuff
+
+(use-package winner
+  :init
+  (winner-mode 1)
+  (setq winner-boring-buffers
+        (append winner-boring-buffers
+                '("*Completions*"
+                  "*Compile-Log*"
+                  "*inferior-lisp*"
+                  "*Fuzzy Completions*"
+                  "*Apropos*"
+                  "*Help*"
+                  "*cvs*"
+                  "*Buffer List*"
+                  "*Ibuffer*"
+                  "*esh command on file*"))))
+
+;;;
+;;; Text wrangling
+
+(defun +align-repeat (start end regexp &optional justify-right after)
+  "Repeat alignment with respect to the given regular expression.
+if JUSTIFY-RIGHT is non nil justify to the right instead of the left. If AFTER is non-nil, add whitespace to the left instead of the right."
+  (interactive "r\nsAlign regexp: ")
+  (let* ((ws-regexp (if (string-empty-p regexp)
+                        "\\(\\s-+\\)"
+                      "\\(\\s-*\\)"))
+         (complete-regexp (if after
+                              (concat regexp ws-regexp)
+                            (concat ws-regexp regexp)))
+         (group (if justify-right -1 1)))
+    (message "%S" complete-regexp)
+    (align-regexp start end complete-regexp group 1 t)))
+
+(defmacro pdc|create-align-repeat-x (name regexp &optional justify-right default-after)
+  (let ((new-func (intern (concat "+align-repeat-" name))))
+    `(defun ,new-func (start end switch)
+       (interactive "r\nP")
+       (let ((after (not (eq (if switch t nil) ,(if default-after t nil)))))
+         (+align-repeat start end ,regexp ,justify-right after)))))
+
+  (defun +align-repeat-decimal (start end)
+    "Align a table of numbers on decimal points and dollar signs (both optional)"
+    (interactive "r")
+    (require 'align)
+    (align-regexp start end nil
+                  '((nil (regexp . "\\([\t ]*\\)\\$?\\([\t ]+[0-9]+\\)\\.?")
+                         (repeat . t)
+                         (group 1 2)
+                         (spacing 1 1)
+                         (justify nil t)))
+                  nil))
+
+(require 'pdcmacs-global-bindings)
+
+(pdc|create-align-repeat-x "comma"     "," nil t)
+(pdc|create-align-repeat-x "semicolon" ";" nil t)
+(pdc|create-align-repeat-x "colon"     ":" nil t)
+(pdc|create-align-repeat-x "equal"     "=")
+(pdc|create-align-repeat-x "math-oper" "[+\\-*/]")
+(pdc|create-align-repeat-x "ampersand" "&")
+(pdc|create-align-repeat-x "bar"       "|")
+(pdc|create-align-repeat-x "left-paren" "(")
+(pdc|create-align-repeat-x "right-paren" ")" t)
+(pdc|create-align-repeat-x "backslash" "\\\\")
+(pdc|create-align-repeat-x "quote" "['`]'")
+
+
+(general-def
+  :prefix "M-m |"
+  ""   '(nil :which-key "align")
+  ","  '+align-repeat-comma
+  ";"  '+align-repeat-semicolon
+  ":"  '+align-repeat-colon
+  "="  '+align-repeat-equal
+  "+"  '+align-repeat-math-oper
+  "*"  '+align-repeat-math-oper
+  "/"  '+align-repeat-math-oper
+  "-"  '+align-repeat-math-oper
+  "|"  '+align-repeat-bar
+  "("  '+align-repeat-left-paren
+  ")"  '+align-repeat-right-paren
+  "\\" '+align-repeat-backslash
+  "'"  '+align-repeat-quote
+  "`"  '+align-repeat-quote)
+
+
+;;; Magit
+(use-package magit
+  :general
+  (:prefix "M-m g"
+           "" '(nil :which-key "Git")
+           "s" 'magit-status))
+
+
+(use-package corfu
+  :custom
+  ;; Works with `indent-for-tab-command'. Make sure tab doesn't indent when you
+  ;; want to perform completion
+  (tab-always-indent 'complete)
+  (completion-cycle-threshold nil)      ; Always show candidates in menu
+
+  (corfu-auto nil)
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.25)
+
+  (corfu-min-width 80)
+  (corfu-max-width corfu-min-width)     ; Always have the same width
+  (corfu-count 14)
+  (corfu-scroll-margin 4)
+  (corfu-cycle nil)
+
+  ;; `nil' means to ignore `corfu-separator' behavior, that is, use the older
+  ;; `corfu-quit-at-boundary' = nil behavior. Set this to separator if using
+  ;; `corfu-auto' = `t' workflow (in that case, make sure you also set up
+  ;; `corfu-separator' and a keybind for `corfu-insert-separator', which my
+  ;; configuration already has pre-prepared). Necessary for manual corfu usage with
+  ;; orderless, otherwise first component is ignored, unless `corfu-separator'
+  ;; is inserted.
+  (corfu-quit-at-boundary nil)
+  (corfu-separator ?\s)            ; Use space
+  (corfu-quit-no-match 'separator) ; Don't quit if there is `corfu-separator' inserted
+  (corfu-preview-current 'insert)  ; Preview first candidate. Insert on input if only one
+  (corfu-preselect-first t)        ; Preselect first candidate?
+
+  ;; Other
+  (corfu-echo-documentation nil)        ; Already use corfu-doc
+  :init
+  (corfu-global-mode))
+
+(use-package yaml-mode
+  :mode "\\.ya?ml\\'"
+  )
+(use-package yaml)
+(use-package flycheck-yamllint)
+
+(use-package clipetty
+  :hook (after-init . global-clipetty-mode))
+
+(use-package yasnippet)
+(use-package consult-yasnippet)
+(use-package yasnippets-orgmode
+  :after org-mode)
+(use-package yasnippets)
+
+(use-package visual-fill-column
+  :init
+  (global-visual-fill-column-mode 1))
+
+(for-terminal
+  (xterm-mouse-mode 1))
+
+(require 'pdcmacs-org)
+
+(require 'pdcmacs-hugo-support)
+(require 'pdcmacs-webservice)
--- early-init.el
+++ early-init.el
@@ -1,61 +1,26 @@
-;;; early-init.el --- Setup straight/use-package -*- lexical-binding: t; -*-
+;;; early-init.el -*- lexical-binding: t; -*-
 
-(setq gc-cons-threshold most-positive-fixnum)
+;; Increase the GC threshold for faster startup
+(setq gc-cons-threshold (* 50 1000 1000))
 
-(setq native-comp-deferred-compilation nil)
+;; Prefer loading the newest compiled .el file
+(setq load-prefer-newer noninteractive)
 
 (setq package-enable-at-startup nil)
 
-(setq load-prefer-newer noninteractive)
-(setq straight-base-dir (expand-file-name ".local/" user-emacs-directory))
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name ".local/straight/repos/straight.el/bootstrap.el"
-			 user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Silence the warnings
+  (setq native-comp-async-report-warnings-errors nil)
 
-(setq straight-use-package-by-default t)
-(straight-use-package 'use-package)
-(straight-use-package 'diminish)
-(straight-use-package 'which-key)
-(straight-use-package 'no-littering)
+  ;; Make it happen asynchronously
+  (setq native-comp-deferred-compilation t)
 
-(unless (or (daemonp) noninteractive)
-  (let ((old-file-name-handler-alist file-name-handler-alist))
-    ;; `file-name-handler-alist' is consulted on each `require', `load' and
-    ;; various path/io functions. You get a minor speed up by unsetting this.
-    (setq-default file-name-handler-alist nil)
-    ;; ...but restore `file-name-handler-alist' later because it's needed for
-    ;; handling encrypted or compressed files, among other things.
-    (defun pdc-reset-file-handler-alist-h ()
-      (setq file-name-handler-alist
-	    ;; Merge don't overwrite because who knows what the rest of the
-	    ;; startup process may have got up to
-	    (delete-dups (append file-name-handler-alist
-				 old-file-name-handler-alist))))
-    (add-hook 'emacs-startup-hook #'pdc-reset-file-handler-alist-h 101))
+  ;; Set the right directory to store the native compilation cache
+  (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory)))
 
-  (setq-default inhibit-redisplay t
-		inhibit-message t)
-  (add-hook 'window-setup-hook
-	    (lambda ()
-	      (setq-default inhibit-redisplay nil
-			    inhibit-message nil)
-	      (redisplay))))
+(setq inhibit-startup-message t)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 
-(set-language-environment "UTF-8")
-
-(setq default-input-method nil)
-
-;; Ensure pdcmacs is running out of this file's directory
-(setq user-emacs-directory (file-name-directory load-file-name))
-
-(load (concat user-emacs-directory "core/core") nil 'nomessage)
